1291d6b2f6ec73c5e2f368552936b14a060cd277
Nick Spain <nicholas.spain96@gmail.com>
2019-09-26 10:31:02 +1000

    Initial commit

---
+++ watchman.el
@@ -0,0 +1,48 @@
+;;; watchman.el --- Interact with watchman from within emacs
+
+;; The MIT License (MIT)
+
+;; Copyright (c) 2019 nick96
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;; Author: Nick Spain <nicholas.spain96@gmail.com>
+;; Version: 0.0.1
+;; Keywords: Productivity
+;; Homepage: https://github.com/nick96/watchman.el
+;; Package-Requires: ()
+
+;;; Commentary
+
+;; Create, inspect, change and delete watchman watches and triggers easily from
+;; within emacs. This package is intended to make basic usage of watchman easy.
+
+;;; Code:
+
+(defgroup watchman nil
+  "Create, inspect, change and delete watchman watches and triggers"
+  :prefix watchman
+  :group 'tools)
+
+(defcustom watchman-executable (executable-find "watchman")
+  "Watchman executable"
+  :group 'watchman
+  :type 'string)
+
+;;; watchman.el ends here
